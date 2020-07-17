a6fb7875c81dd98c4db2ded186bab07648031e60
Daniel Couch <couchd@musc.edu>
2020-07-16 23:12:03 -0400

    first push

---
+++ Assets/Grounded.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Grounded : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When a collision happens, the function OnCollisionEnter2D is called.
+    private void OnCollisionEnter2D(Collision2D collision) {
+        // Check what the tag of the incoming collider is.
+        if (collision.collider.tag == "Ground") {
+            Debug.Log("here!!!");
+            Player.GetComponent<Move2D>().isGrounded = true;
+            Debug.Log(Player.GetComponent<Move2D>().animator);
+            Player.GetComponent<Move2D>().animator.SetBool("IsJumping", false);
+            
+        }
+    }
+    // When the collision ends, the function OnCollisionExit2D is called.
+    private void OnCollisionExit2D(Collision2D collision) {
+        if (collision.collider.tag == "Ground") {
+            Player.GetComponent<Move2D>().isGrounded = false;
+        }
+    }
+}
---
+++ Assets/PlayerController01.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // How fast the Player is moving
+    public float speed;
+    
+    public Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Getting the Rigidbody component associated with the Player object.
+        rb = GetComponent<Rigidbody2D>();
+        speed = 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GetAxis horizontal: checks to see if user is moving left or right, and is equal to -1 or 1
+        // GetAxis vertical: check to see if user is moving down or up, and is equal to -1 or 1
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        // For instance, user pushing right arrow key and the up arrow key,
+        // Then moveInput = <1, 1>
+        // If they were pushing left arrow key and up arrow key,
+        // Then moveInput = <-1, 1>
+        // If they were pushing right arrow key and the down arrow key,
+        // Then moveInput = <1, -1>
+        // If they were pushing left arrow key and the down arrow key,
+        // Then moveInput = <-1, -1>
+        // If they're not moving
+        // Then moveInput = <0, 0>
+
+        // moveVelocity represe
+        moveVelocity = moveInput.normalized * speed;
+        animator.SetFloat("Speed", moveVelocity.magnitude);
+    }
+
+    // Code related to physics
+    //void FixedUpdate() {
+    //    rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    //}
+}
